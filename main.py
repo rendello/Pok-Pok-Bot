b13b9b14415f40bcc591315e19595512523bdffa
Gaven Rendell <gavenrendell@protonmail.com>
2019-10-06 16:39:51 -0400

    adds get_random_pokemon(); fetch_image()

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3.6
+
+import random
+
+import urllib.request 
+from PIL import Image
+
+from db_context_manager import dbopen
+
+
+def get_random_pokemon():
+    '''
+    Args:
+        None
+
+    Returns:
+        pokemon: a <dict> with 'pokemon' (the creature's name <str>) and 'id' (its id <str>).
+    '''
+    with dbopen('pokemon.db') as c:
+
+        # Must use rowid, as id is technically text.
+        c.execute('SELECT MAX(rowid) FROM pokemon;')
+        highest_id = c.fetchone()[0]
+
+        pokemon_id = random.randint(1, highest_id)
+        c.execute('SELECT pokemon, id FROM pokemon WHERE id=?;', [str(pokemon_id)])
+        result = c.fetchone()
+
+        pokemon = {'pokemon': result[0], 'id': result[1]}
+
+        return pokemon
+
+
+def fetch_image(pokemon_id):
+    url = f'https://assets.pokemon.com/assets/cms2/img/pokedex/full/{pokemon_id}.png'
+
+    # The first value is the path to the image's tempfile.
+    image_path = urllib.request.urlretrieve(url)[0]
+
+    i = Image.open(image_path)
+    i.show()
+
+    
+pokemon = get_random_pokemon()
+fetch_image(pokemon['id'])
+
+
+
