6bc953b8495fcc0cc290a1d26944ce6cd86a62c3
Gaven Rendell <gavenrendell@protonmail.com>
2019-10-06 12:09:34 -0400

    two layers on pokemon image

--- who_is_she.py
+++ who_is_she.py
@@ -3,7 +3,7 @@
 from PIL import Image
 import numpy as np
 
-def poke_png_to_sillhouette(image_path):
+def poke_png_to_sillhouette(image_path, color_tuple):
     ''' Flattens all non transparent pixels to the classic "who's that pokemon" blue.
 
         Code modified from this thread: https://stackoverflow.com/a/3753428
@@ -15,10 +15,16 @@ def poke_png_to_sillhouette(image_path):
     _, _, _, alpha = data.T
 
     non_transparent_areas = (alpha != 1)
-    data[..., :-1][non_transparent_areas.T] = (13, 93, 164)
+    data[..., :-1][non_transparent_areas.T] = color_tuple
 
     im2 = Image.fromarray(data)
-    im2.show()
+    #im2.show()
+
+    return im2
 
 
-poke_png_to_sillhouette('img.png')
+im1 = poke_png_to_sillhouette('img.png', (0,0,0))
+im2 = poke_png_to_sillhouette('img.png', (13, 93, 164))
+
+im1.paste(im2, (7, 0), im2)
+im1.show()
