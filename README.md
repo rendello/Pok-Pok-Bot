e25ac9b2fe0c31dfe56380681803432220e97e36
Gaven Rendell <gaven@rendello.ca>
2019-11-02 14:21:40 -0400

    merges dev, code style now Black

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 # Poké Poké: *Who's That Pokémon* for Discord!
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 ![header](Artwork/header.png "header")
 
 The classic Pokémon guessing game. Simple and fun!
--- poke/core.py
+++ poke/core.py
@@ -12,28 +12,28 @@ from helpers import get_pokemon_and_image, config
 
 # ------ User functions --------
 def clean_input_string(input_string):
-    ''' Replaces unwanted characters with spaces.
+    """ Replaces unwanted characters with spaces.
     
     Args:
         input_string (str): A string to be cleaned.
 
     Returns:
         str: A string containing only the accepted characters.
-    '''
+    """
     input_string = input_string.lower()
-    accepted_chars = 'abcdefghijklmnopqrstuvwxyz: é'
+    accepted_chars = "abcdefghijklmnopqrstuvwxyz: é"
     clean_string = str()
 
     for char in input_string:
         if char not in accepted_chars:
-            clean_string += ' '
+            clean_string += " "
         else:
             clean_string += char
     return clean_string
 
 
 def pokemon_in_text(*, text, pokemon_name):
-    ''' Finds the given pokemon_name in the given text.
+    """ Finds the given pokemon_name in the given text.
     
     Technically  pokemon_name can be any string. Spelling doesn't need to perfect, as the
     function uses fuzzy searching.
@@ -44,7 +44,7 @@ def pokemon_in_text(*, text, pokemon_name):
 
     Returns:
         bool: True if found (fuzzily), False if not
-    '''
+    """
     pokemon_name = pokemon_name.lower()
 
     if len(text) > 100:
@@ -77,7 +77,7 @@ def too_many_matches_for_user(user):
 
 
 def extract_generations(generation_string):
-    ''' Extracts generations and ranges from strings in format 'n' or 'min-max'.
+    """ Extracts generations and ranges from strings in format 'n' or 'min-max'.
 
     Args:
         generation_string (str): String that might be in format 'n' or 'min-max', eg. if the
@@ -90,12 +90,12 @@ def extract_generations(generation_string):
 
     Returns:
         list: Holds the gen number, or range of gens, or empty.
-    '''
+    """
 
     all_generations = [1, 2, 3, 4, 5, 6, 7]
 
     # string = 'all'
-    if generation_string == 'all':
+    if generation_string == "all":
         return all_generations
 
     # format = 'n'
@@ -108,7 +108,7 @@ def extract_generations(generation_string):
 
     # format = 'min-max'
     try:
-        lower_bound, upper_bound = [int(i) for i in generation_string.split('-')]
+        lower_bound, upper_bound = [int(i) for i in generation_string.split("-")]
 
         min_ = min(all_generations)
         if lower_bound < min_:
@@ -128,23 +128,23 @@ def extract_generations(generation_string):
 
 
 async def change_status_task(primary_status, secondary_statuses):
-    ''' Replaces the primary status with secondary ones ar small intervals.
+    """ Replaces the primary status with secondary ones ar small intervals.
 
     Args:
         primary_status (str): The 'game' that will be shown as a Discord status most often.
         secondary_statuses (list): Strings containing less important statuses.
-    '''
+    """
 
     while True:
         for secondary_status in secondary_statuses:
-            await bot.change_presence(activity=discord.Game('!poke  |  !poke-help'))
+            await bot.change_presence(activity=discord.Game("!poke  |  !poke-help"))
             await asyncio.sleep(30)
             await bot.change_presence(activity=discord.Game(secondary_status))
             await asyncio.sleep(10)
 
 
 async def can_start_match(server_id, channel_id, user_id):
-    ''' Checks if a requested match can be started. '''
+    """ Checks if a requested match can be started. """
     global matches
 
     if channel_id not in matches:
@@ -154,10 +154,9 @@ async def can_start_match(server_id, channel_id, user_id):
     return False
 
 
-
 # ---------- Classes -----------
-class Match():
-    ''' A given game of WTP.
+class Match:
+    """ A given game of WTP.
 
     Attributes:
         ctx (discord.ext.commands.context.Context): Discord context.
@@ -174,7 +173,7 @@ class Match():
 
         messages (dict): Contains 'sections' as keys, and 'text' for each section as
             values. To be used by send_message and related functions.
-    '''
+    """
 
     def __init__(self, ctx, generation_string):
         self.ctx = ctx
@@ -185,56 +184,55 @@ class Match():
         self.cancellation_aborted = False
 
         poke_data = get_pokemon_and_image(self.generations)
-        self.pokemon_name = poke_data['name']
-        self.unshrouded_path = poke_data['unshrouded_path']
-        self.shrouded_path = poke_data['shrouded_path']
-        
+        self.pokemon_name = poke_data["name"]
+        self.unshrouded_path = poke_data["unshrouded_path"]
+        self.shrouded_path = poke_data["shrouded_path"]
+
         self.messages = {}
 
         print(self.pokemon_name)
 
-
     async def send_message(self, text=str(), *, section, file=None):
-        ''' Sends a message, or replaces the message if the section is already used. '''
+        """ Sends a message, or replaces the message if the section is already used. """
         if section in self.messages.keys():
             _id = await self.messages[section].edit(content=text)
         else:
             _id = await self.ctx.send(text, file=file)
             self.messages[section] = _id
 
-
     async def append_to_message(self, *, text, section):
-        ''' Appends to message. If a full edit needed, see send_message. '''
+        """ Appends to message. If a full edit needed, see send_message. """
         message = self.messages[section]
-        await message.edit(content=f'{message.content}{text}')
-
+        await message.edit(content=f"{message.content}{text}")
 
     async def set_timer(self, seconds):
         await asyncio.sleep(seconds)
 
         if not self.match_ended:
-            await self.end('failure')
-
+            await self.end("failure")
 
     async def ask_to_cancel(self, trigger_message):
-        ''' Starts a cancellation timer to end match, allows users to cancel cancellation.
+        """ Starts a cancellation timer to end match, allows users to cancel cancellation.
 
         Args:
             trigger_message (str): The message that triggered the cancellation process.
-        '''
+        """
 
         # If a cancellation has already been vetoed, no use calling another cancellation
         # when there's so little time in a match anyway.
         if self.cancellation_aborted == False:
-            message_text = '```Cancelling match in 5 seconds. Press on ❌ react to stop cancellation.\n' \
-                    '(cancellations can only be called once per match).```'
-            await self.send_message(f"> {trigger_message}\n{message_text}", section='cancel_dialogue')
-            await self.messages['cancel_dialogue'].add_reaction('❌')
+            message_text = (
+                "```Cancelling match in 5 seconds. Press on ❌ react to stop cancellation.\n"
+                "(cancellations can only be called once per match).```"
+            )
+            await self.send_message(
+                f"> {trigger_message}\n{message_text}", section="cancel_dialogue"
+            )
+            await self.messages["cancel_dialogue"].add_reaction("❌")
             await asyncio.sleep(5)
-            await self.messages['cancel_dialogue'].delete()
+            await self.messages["cancel_dialogue"].delete()
             if self.cancellation_aborted == False:
-                await self.end('failure')
-
+                await self.end("failure")
 
     async def start(self):
         global current_servers
@@ -249,35 +247,44 @@ class Match():
             "Are you ready for",
             f"{self.ctx.message.author.name}! I choose you to play",
             "New Pokémon, who dis?",
-            "Hi! You have 35 second to tell me"
+            "Hi! You have 35 second to tell me",
         ]
 
         intro_text = f"*{random.choice(silly_intro_texts)}* ***WHO'S THAT POKÉMON?***"
 
-        await self.send_message(text=intro_text, file=discord.File(self.shrouded_path), section='shrouded_image')
+        await self.send_message(
+            text=intro_text,
+            file=discord.File(self.shrouded_path),
+            section="shrouded_image",
+        )
         await self.set_timer(35)
 
-
     async def end(self, nature, winner=str()):
-        ''' Ends a match.
+        """ Ends a match.
 
         Sends a win or lose message, deletes the shrouded message, and deletes the match
         reference in matches (also current_users and current_servers).
 
         Args:
             nature (str): 'success' if match won in time, 'failure' if not.
-        '''
+        """
         self.match_ended = True
 
-        if nature == 'failure':
-            await self.send_message(f"Time's up! It's {self.pokemon_name}!",
-                    section='end', file=discord.File(self.unshrouded_path))
+        if nature == "failure":
+            await self.send_message(
+                f"Time's up! It's {self.pokemon_name}!",
+                section="end",
+                file=discord.File(self.unshrouded_path),
+            )
 
-        elif nature == 'success':
-            await self.send_message(f"That's right, {winner.mention}! It's {self.pokemon_name}!",
-                    section='end', file=discord.File(self.unshrouded_path))
+        elif nature == "success":
+            await self.send_message(
+                f"That's right, {winner.mention}! It's {self.pokemon_name}!",
+                section="end",
+                file=discord.File(self.unshrouded_path),
+            )
 
-        await self.messages['shrouded_image'].delete()
+        await self.messages["shrouded_image"].delete()
 
         del matches[self.channel.id]
         if self.ctx.guild is not None:
@@ -285,9 +292,8 @@ class Match():
         current_users.remove(self.ctx.message.author.id)
 
 
-
 # ---------- Globals -----------
-client_secret = open(config['token_dir']).read().replace('\n', '')
+client_secret = open(config["token_dir"]).read().replace("\n", "")
 
 bot = commands.Bot(command_prefix="!")
 
@@ -298,7 +304,7 @@ matches = {}
 current_servers = []
 current_users = []
 
-primary_status = '!poke | !poke-help'
+primary_status = "!poke | !poke-help"
 secondary_statuses = [
     "Who's That Pokemon?",
     "gitlab.com/rendello/",
@@ -306,38 +312,43 @@ secondary_statuses = [
 ]
 
 
-
 # ---------- Commands ----------
-bot.remove_command('help')
+bot.remove_command("help")
 
-@bot.command(aliases=['p'])
-async def poke(ctx, *, generation_string='all'):
+
+@bot.command(aliases=["p"])
+async def poke(ctx, *, generation_string="all"):
 
     # No server or channel IDs when being DM-ed
     server_id = ctx.guild.id if ctx.guild is not None else None
     channel_id = ctx.channel.id if ctx.channel is not None else None
 
-    if await can_start_match(server_id=server_id, channel_id=channel_id, user_id=ctx.message.author.id):
+    if await can_start_match(
+        server_id=server_id, channel_id=channel_id, user_id=ctx.message.author.id
+    ):
 
-        matches[ctx.message.channel.id] = Match(ctx, generation_string=generation_string)
+        matches[ctx.message.channel.id] = Match(
+            ctx, generation_string=generation_string
+        )
         await matches[ctx.message.channel.id].start()
 
 
-@bot.command(aliases=['h', 'poke-help', 'poke-h'])
+@bot.command(aliases=["h", "poke-help", "poke-h"])
 async def help(ctx):
-    await ctx.channel.send('```Pokebot is the "Who\'s That Pokemon" bot!\n\n'
-            + '!poke       Play a match\n'
-            + '!poke 1     Only use generation 1 Pokemon\n'
-            + '!poke 2-4   Use generations 2 through 4\n\n'
-            + '!poke-help  Show this dialogue\n\n'
-            + 'type "cancel" or "idk" to cancel a match.```')
+    await ctx.channel.send(
+        '```Pokebot is the "Who\'s That Pokemon" bot!\n\n'
+        + "!poke       Play a match\n"
+        + "!poke 1     Only use generation 1 Pokemon\n"
+        + "!poke 2-4   Use generations 2 through 4\n\n"
+        + "!poke-help  Show this dialogue\n\n"
+        + 'type "cancel" or "idk" to cancel a match.```'
+    )
 
 
 @bot.command()
 async def d(ctx):
     print(bot.guilds)
     print(matches)
-
 
 
 # ----------- Events -----------
@@ -348,12 +359,12 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    ''' Checks for pokemon in messages after matches have been called.
+    """ Checks for pokemon in messages after matches have been called.
 
     Args:
         message (discord.message.Message): automatically passed for every message of
         every connected server.
-    '''
+    """
 
     # Only look for pokemon in channels with matches
     if (message.channel.id in matches.keys()) and (message.author.id != bot.user.id):
@@ -362,8 +373,8 @@ async def on_message(message):
         clean_message = clean_input_string(message.content)
 
         if pokemon_in_text(text=clean_message, pokemon_name=match.pokemon_name):
-            await match.end('success', winner=message.author)
-        elif any(word in clean_message for word in ['cancel', 'idk']):
+            await match.end("success", winner=message.author)
+        elif any(word in clean_message for word in ["cancel", "idk"]):
             await match.ask_to_cancel(trigger_message=message.content)
 
     # Stops on_message from blocking all other commands.
@@ -378,15 +389,16 @@ async def on_reaction_add(reaction, user):
     if reaction.message.channel.id in matches.keys() and user.id != bot.user.id:
         match = matches[reaction.message.channel.id]
 
-        if 'cancel_dialogue' in match.messages:
-            if reaction.message.id == match.messages['cancel_dialogue'].id:
+        if "cancel_dialogue" in match.messages:
+            if reaction.message.id == match.messages["cancel_dialogue"].id:
                 match.cancellation_aborted = True
 
                 # Bad
-                await match.send_message('Cancellation aborted. ', section='cancel_dialogue')
-
+                await match.send_message(
+                    "Cancellation aborted. ", section="cancel_dialogue"
+                )
 
 
 # ------- Program Start --------
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.run(client_secret)
